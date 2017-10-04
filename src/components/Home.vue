a07f81564a23a2ecf2c499f34fb6397c552221e8
BBJI <237024726@qq.com>
2017-10-04 19:21:07 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<p>Yours Funds: {{ funds | currency }}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+	    computed: {
+	        funds() {
+                return this.$store.getters.funds;
+            }
+		}
+	}
+</script>
+
+<style>
+</style>
